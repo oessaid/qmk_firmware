7afc840494df0bdc2181b99518071a9d23f45cba
Omar Essaid <omar.essaid@helsing.ai>
2023-04-03 13:10:14 +0200

    Focus on thumbs

--- keyboards/planck/keymaps/oessaid/keymap.c
+++ keyboards/planck/keymaps/oessaid/keymap.c
@@ -24,7 +24,8 @@ enum planck_layers {
   _FUNC,
   _VIM,
   _TMUX,
-  _ADJUST
+  _ADJUST,
+  _HYPER
 };
 
 // clang-format off
@@ -35,6 +36,7 @@ enum planck_keycodes {
   FUNC,
   VIM,
   TMUX,
+  HYPER,
   BACKLIT,
   EXT_PLV,
   ST_MACRO_0,
@@ -89,59 +91,35 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_QWERTY] = LAYOUT_planck_grid(
     KC_TAB,    KC_Q,      KC_W,     KC_E,     KC_R,      KC_T,      KC_Y,            KC_U,   KC_I,         KC_O,      KC_P,       LT(6, KC_ESCAPE),      
-    KC_BSPACE, KC_A,      KC_S,     KC_D,     KC_F,      KC_G,      KC_H,            KC_J,   KC_K,         KC_L,      KC_SCOLON,  KC_QUOTE,       
+    KC_BSPACE, KC_A,      KC_S,     KC_D,     LT(2, KC_F),      KC_G,      KC_H,            KC_J,   KC_K,         KC_L,      KC_SCOLON,  KC_QUOTE,       
     KC_LSPO,   KC_Z,      KC_X,     KC_C,     KC_V,      KC_B,      KC_N,            KC_M,   KC_COMMA,     KC_DOT,    KC_SLASH,   KC_RSPC,        
-    MO(4),     KC_LCTRL,  KC_LALT,  KC_HYPR,    KC_LGUI,   KC_SPACE,  LT(1,KC_SPACE),  MO(2),  KC_CAPSLOCK,  KC_RALT,   KC_RCTRL,   LT(5,KC_ENTER)
+    /* MO(4),     KC_LCTRL,  KC_LALT,  KC_HYPR,    KC_LGUI,   KC_SPACE,  LT(1,KC_SPACE),  MO(2),  KC_CAPSLOCK,  KC_RALT,   KC_RCTRL,   LT(5,KC_ENTER) */
+    MO(3),     KC_LCTRL,  KC_LALT,  KC_LGUI,  LT(4, KC_ENTER), LT(1, KC_BSPACE),    LT(1, KC_SPACE), LT(5, KC_ENTER),  KC_CAPSLOCK,  KC_RALT,   KC_RCTRL,  MO(3) 
+    /* _______, _______, _______, _______,    SYM+NUM,          VIM,             TMUX,           NAV,          _______,  _______, _______, _______ */
 ),
+/* 1: SYMBOLS+NUM; 
+ * 2: NAV; 
+ * 3: FN; 
+ * 4: VIM; 
+ * 5: TMUX 
+ * 7: HYPER */
 
-/* Symbols
- * ,-----------------------------------------------------------------------------------.
- * | Tab  |   Q  |   W  |   F  |   P  |   G  |   J  |   L  |   U  |   Y  |   ;  | Bksp |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Esc  |   A  |   R  |   S  |   T  |   D  |   H  |   N  |   E  |   I  |   O  |  '   |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Shift|   Z  |   X  |   C  |   V  |   B  |   K  |   M  |   ,  |   .  |   /  |Enter |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Brite| Ctrl | Alt  | GUI  |Lower |    Space    |Raise | Left | Down |  Up  |Right |
- * `-----------------------------------------------------------------------------------'
- */
 [_SYMBOLS] = LAYOUT_planck_grid(
     KC_GRAVE, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_LBRACKET, KC_RBRACKET, KC_7,    KC_8,     KC_9,   KC_MINUS, KC_EQUAL,       
     _______ , KC_CIRC, KC_AMPR, KC_ASTR, KC_PERC, KC_LPRN,     KC_RPRN,     KC_4,    KC_5,     KC_6,   KC_0,     KC_BSLASH,      
     _______ , _______, _______, _______, _______, KC_LCBR,     KC_RCBR,     KC_1,    KC_2,     KC_3,   _______,  _______, 
     _______ , _______, _______, _______, KC_LABK, _______,     _______,     KC_RABK, KC_COMMA, KC_DOT, _______,  KC_F10
+    /* _______ , _______, _______, _______, _______, KC_LABK,     KC_RABK,     _______, KC_COMMA, KC_DOT, _______,  KC_F10 */
 ),
 
-/* Navigation
- * ,-----------------------------------------------------------------------------------.
- * | Tab  |   '  |   ,  |   .  |   P  |   Y  |   F  |   G  |   C  |   R  |   L  | Bksp |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Esc  |   A  |   O  |   E  |   U  |   I  |   D  |   H  |   T  |   N  |   S  |  /   |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Shift|   ;  |   Q  |   J  |   K  |   X  |   B  |   M  |   W  |   V  |   Z  |Enter |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Brite| Ctrl | Alt  | GUI  |Lower |    Space    |Raise | Left | Down |  Up  |Right |
- * `-----------------------------------------------------------------------------------'
- */
 [_NAVIGATION] = LAYOUT_planck_grid(
 
     _______, _______, _______, _______, _______, _______,   _______, LCTL(LSFT(KC_TAB)), LCTL(KC_TAB), _______,  _______,         _______, 
     _______, _______, _______, _______, _______, _______,   KC_LEFT, KC_DOWN,            KC_UP,        KC_RIGHT, LGUI(KC_TAB), LGUI(KC_GRAVE),
     _______, _______, _______, _______, _______, _______,   _______, _______,            _______,      _______,  _______,         _______, 
-    _______, _______, _______, _______, _______, KC_DELETE, _______, _______,            _______,      _______,  _______,         _______
+    _______, _______, _______, _______, _______, _______, _______, _______,            _______,      _______,  _______,         _______
 ),
 
-/* Lower
- * ,-----------------------------------------------------------------------------------.
- * |   ~  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  | Bksp |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   _  |   +  |   {  |   }  |  |   |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |ISO ~ |ISO | | Home | End  |      |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |             |      | Next | Vol- | Vol+ | Play |
- * `-----------------------------------------------------------------------------------'
- */
 [_FUNC] = LAYOUT_planck_grid(
 
     _______, _______, _______, _______, _______, _______, _______, KC_F7,   KC_F8,   KC_F9,   KC_F10, _______, 
@@ -150,40 +128,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, _______, _______, _______, _______, _______, KC_NO,   _______, _______, _______, _______, _______
 ),
 
-/* Raise
- * ,-----------------------------------------------------------------------------------.
- * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Bksp |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   -  |   =  |   [  |   ]  |  \   |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |ISO # |ISO / |Pg Up |Pg Dn |      |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |             |      | Next | Vol- | Vol+ | Play |
- * `-----------------------------------------------------------------------------------'
- */
 [_VIM] = LAYOUT_planck_grid(
     _______, _______, _______, _______, _______,     _______, _______,     ST_MACRO_22, ST_MACRO_23, _______,     _______, _______, 
     _______, _______, _______, _______, _______,     _______, ST_MACRO_24, ST_MACRO_25, ST_MACRO_26, ST_MACRO_27, _______, _______, 
     _______, _______, _______, _______, _______,     _______, _______,     _______,     _______,     _______,     _______, _______, 
-    KC_NO,   _______, _______, _______, ST_MACRO_28, _______, KC_NO,       ST_MACRO_29, _______,     _______,     _______, _______
+    KC_NO,   _______, _______, _______, _______, ST_MACRO_28, ST_MACRO_29, MO(7), _______,     _______,     _______, _______
+    /* KC_NO,   _______, _______, _______, ST_MACRO_28, _______, KC_HYPR,       ST_MACRO_29, _______,     _______,     _______, _______ */
 ),
 
-/* Plover layer (http://opensteno.org)
- * ,-----------------------------------------------------------------------------------.
- * |   #  |   #  |   #  |   #  |   #  |   #  |   #  |   #  |   #  |   #  |   #  |   #  |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |   S  |   T  |   P  |   H  |   *  |   *  |   F  |   P  |   L  |   T  |   D  |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |   S  |   K  |   W  |   R  |   *  |   *  |   R  |   B  |   G  |   S  |   Z  |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Exit |      |      |   A  |   O  |             |   E  |   U  |      |      |      |
- * `-----------------------------------------------------------------------------------'
- */
 [_TMUX] = LAYOUT_planck_grid(
     _______, _______,     ST_MACRO_0,  ST_MACRO_1,  ST_MACRO_2,  _______, _______,    ST_MACRO_3,  ST_MACRO_4,  _______,     _______,     ST_MACRO_5,     
-    _______, _______,     ST_MACRO_6,  ST_MACRO_7,  ST_MACRO_8,  _______, ST_MACRO_9, ST_MACRO_10, ST_MACRO_11, ST_MACRO_12, _______,     ST_MACRO_13,    
+    _______, _______,     ST_MACRO_6,  ST_MACRO_7,  ST_MACRO_8,  _______, ST_MACRO_9, ST_MACRO_10, ST_MACRO_11, ST_MACRO_12, LGUI(KC_TAB),     ST_MACRO_13,    
     _______, ST_MACRO_14, ST_MACRO_15, ST_MACRO_16, _______,     _______, _______,    _______,     ST_MACRO_17, ST_MACRO_18, ST_MACRO_19, _______, 
-    _______, _______,     _______,     _______,     ST_MACRO_20, _______, KC_NO,      ST_MACRO_21, _______,     _______,     _______,     _______
+    _______, _______,     _______,     _______,     MO(7), ST_MACRO_20, ST_MACRO_21, _______, _______,     _______,     _______,     _______
+    /* _______, _______,     _______,     _______,     ST_MACRO_20, KC_HYPR, _______,      ST_MACRO_21, _______,     _______,     _______,     _______ */
 ),
 
 /* Adjust (Lower + Raise)
@@ -203,7 +161,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, _______, MU_MOD,  AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, QWERTY,  SYMBOLS,  NAVIGATION,  TMUX,  _______,
     _______, MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  TERM_ON, TERM_OFF, _______, _______, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______
-)
+),
+
+[_HYPER] = LAYOUT_planck_grid(
+
+    _______, _______, _______, _______, _______, _______, _______, HYPR(KC_U),   HYPR(KC_I), _______, _______, HYPR(KC_EQUAL), 
+    _______, _______, _______, _______, _______, _______, HYPR(KC_H), HYPR(KC_J), HYPR(KC_K), HYPR(KC_L), HYPR(KC_SCOLON), _______, 
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, 
+    _______, _______, _______, _______, _______, _______, KC_NO,   _______, _______, _______, _______, _______
+),
 
 };
 
@@ -211,133 +177,133 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
     case ST_MACRO_0:
     if (record->event.pressed) {
-      SEND_STRING(SS_LCTL(SS_TAP(X_SPACE)) SS_DELAY(100) SS_TAP(X_W));
+      SEND_STRING(SS_LCTL(SS_TAP(X_SPACE)) SS_DELAY(50) SS_TAP(X_W));
 
     }
     break;
     case ST_MACRO_1:
     if (record->event.pressed) {
-      SEND_STRING(SS_LCTL(SS_TAP(X_SPACE)) SS_DELAY(100) SS_LSFT(SS_TAP(X_E)));
+      SEND_STRING(SS_LCTL(SS_TAP(X_SPACE)) SS_DELAY(50) SS_LSFT(SS_TAP(X_E)));
 
     }
     break;
     case ST_MACRO_2:
     if (record->event.pressed) {
-      SEND_STRING(SS_LCTL(SS_TAP(X_SPACE)) SS_DELAY(100) SS_LSFT(SS_TAP(X_4)));
+      SEND_STRING(SS_LCTL(SS_TAP(X_SPACE)) SS_DELAY(50) SS_LSFT(SS_TAP(X_4)));
 
     }
     break;
     case ST_MACRO_3:
     if (record->event.pressed) {
-      SEND_STRING(SS_LCTL(SS_TAP(X_SPACE)) SS_DELAY(100) SS_TAP(X_P));
+      SEND_STRING(SS_LCTL(SS_TAP(X_SPACE)) SS_DELAY(50) SS_TAP(X_P));
 
     }
     break;
     case ST_MACRO_4:
     if (record->event.pressed) {
-      SEND_STRING(SS_LCTL(SS_TAP(X_SPACE)) SS_DELAY(100) SS_TAP(X_N));
+      SEND_STRING(SS_LCTL(SS_TAP(X_SPACE)) SS_DELAY(50) SS_TAP(X_N));
 
     }
     break;
     case ST_MACRO_5:
     if (record->event.pressed) {
-      SEND_STRING(SS_LCTL(SS_TAP(X_SPACE)) SS_DELAY(100) SS_LSFT(SS_TAP(X_7)));
+      SEND_STRING(SS_LCTL(SS_TAP(X_SPACE)) SS_DELAY(50) SS_LSFT(SS_TAP(X_7)));
 
     }
     break;
     case ST_MACRO_6:
     if (record->event.pressed) {
-      SEND_STRING(SS_LCTL(SS_TAP(X_SPACE)) SS_DELAY(100) SS_TAP(X_S));
+      SEND_STRING(SS_LCTL(SS_TAP(X_SPACE)) SS_DELAY(50) SS_TAP(X_S));
 
     }
     break;
     case ST_MACRO_7:
     if (record->event.pressed) {
-      SEND_STRING(SS_LCTL(SS_TAP(X_SPACE)) SS_DELAY(100) SS_TAP(X_D));
+      SEND_STRING(SS_LCTL(SS_TAP(X_SPACE)) SS_DELAY(50) SS_TAP(X_D));
 
     }
     break;
     case ST_MACRO_8:
     if (record->event.pressed) {
-      SEND_STRING(SS_LCTL(SS_TAP(X_SPACE)) SS_DELAY(100) SS_TAP(X_F));
+      SEND_STRING(SS_LCTL(SS_TAP(X_SPACE)) SS_DELAY(50) SS_TAP(X_F));
 
     }
     break;
     case ST_MACRO_9:
     if (record->event.pressed) {
-      SEND_STRING(SS_LCTL(SS_TAP(X_SPACE)) SS_DELAY(100) SS_TAP(X_H));
+      SEND_STRING(SS_LCTL(SS_TAP(X_SPACE)) SS_DELAY(50) SS_TAP(X_H));
 
     }
     break;
     case ST_MACRO_10:
     if (record->event.pressed) {
-      SEND_STRING(SS_LCTL(SS_TAP(X_SPACE)) SS_DELAY(100) SS_TAP(X_J));
+      SEND_STRING(SS_LCTL(SS_TAP(X_SPACE)) SS_DELAY(50) SS_TAP(X_J));
 
     }
     break;
     case ST_MACRO_11:
     if (record->event.pressed) {
-      SEND_STRING(SS_LCTL(SS_TAP(X_SPACE)) SS_DELAY(100) SS_TAP(X_K));
+      SEND_STRING(SS_LCTL(SS_TAP(X_SPACE)) SS_DELAY(50) SS_TAP(X_K));
 
     }
     break;
     case ST_MACRO_12:
     if (record->event.pressed) {
-      SEND_STRING(SS_LCTL(SS_TAP(X_SPACE)) SS_DELAY(100) SS_TAP(X_L));
+      SEND_STRING(SS_LCTL(SS_TAP(X_SPACE)) SS_DELAY(50) SS_TAP(X_L));
 
     }
     break;
     case ST_MACRO_13:
     if (record->event.pressed) {
-      SEND_STRING(SS_LCTL(SS_TAP(X_SPACE)) SS_DELAY(100) SS_TAP(X_EQUAL));
+      SEND_STRING(SS_LCTL(SS_TAP(X_SPACE)) SS_DELAY(50) SS_TAP(X_EQUAL));
 
     }
     break;
     case ST_MACRO_14:
     if (record->event.pressed) {
-      SEND_STRING(SS_LCTL(SS_TAP(X_SPACE)) SS_DELAY(100) SS_TAP(X_Z));
+      SEND_STRING(SS_LCTL(SS_TAP(X_SPACE)) SS_DELAY(50) SS_TAP(X_Z));
 
     }
     break;
     case ST_MACRO_15:
     if (record->event.pressed) {
-      SEND_STRING(SS_LCTL(SS_TAP(X_SPACE)) SS_DELAY(100) SS_TAP(X_X));
+      SEND_STRING(SS_LCTL(SS_TAP(X_SPACE)) SS_DELAY(50) SS_TAP(X_X));
 
     }
     break;
     case ST_MACRO_16:
     if (record->event.pressed) {
-      SEND_STRING(SS_LCTL(SS_TAP(X_SPACE)) SS_DELAY(100) SS_TAP(X_C));
+      SEND_STRING(SS_LCTL(SS_TAP(X_SPACE)) SS_DELAY(50) SS_TAP(X_C));
 
     }
     break;
     case ST_MACRO_17:
     if (record->event.pressed) {
-      SEND_STRING(SS_LCTL(SS_TAP(X_SPACE)) SS_DELAY(100) SS_TAP(X_COMMA));
+      SEND_STRING(SS_LCTL(SS_TAP(X_SPACE)) SS_DELAY(50) SS_TAP(X_COMMA));
 
     }
     break;
     case ST_MACRO_18:
     if (record->event.pressed) {
-      SEND_STRING(SS_LCTL(SS_TAP(X_SPACE)) SS_DELAY(100) SS_TAP(X_LBRACKET));
+      SEND_STRING(SS_LCTL(SS_TAP(X_SPACE)) SS_DELAY(50) SS_TAP(X_LBRACKET));
 
     }
     break;
     case ST_MACRO_19:
     if (record->event.pressed) {
-      SEND_STRING(SS_LCTL(SS_TAP(X_SPACE)) SS_DELAY(100) SS_TAP(X_RBRACKET));
+      SEND_STRING(SS_LCTL(SS_TAP(X_SPACE)) SS_DELAY(50) SS_TAP(X_RBRACKET));
 
     }
     break;
     case ST_MACRO_20:
     if (record->event.pressed) {
-      SEND_STRING(SS_LCTL(SS_TAP(X_SPACE)) SS_DELAY(100) SS_TAP(X_MINUS));
+      SEND_STRING(SS_LCTL(SS_TAP(X_SPACE)) SS_DELAY(50) SS_TAP(X_MINUS));
 
     }
     break;
     case ST_MACRO_21:
     if (record->event.pressed) {
-      SEND_STRING(SS_LCTL(SS_TAP(X_SPACE)) SS_DELAY(100) SS_TAP(X_BSLASH));
+      SEND_STRING(SS_LCTL(SS_TAP(X_SPACE)) SS_DELAY(50) SS_TAP(X_BSLASH));
 
     }
     break;
